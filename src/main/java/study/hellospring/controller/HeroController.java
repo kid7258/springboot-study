462d6d7eb7a1a8dce5a771f78d441d2d9f94202e
kid7258 <kid7258@naver.com>
2020-11-11 00:24:54 +0900

    코드 리뷰 내용 반영

--- src/main/java/study/hellospring/controller/HeroController.java
+++ src/main/java/study/hellospring/controller/HeroController.java
@@ -1,15 +1,13 @@
 package study.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import study.hellospring.domain.Hero;
 import study.hellospring.service.HeroService;
 
 import java.util.List;
-import java.util.Optional;
 
-@Controller
+@RestController
 public class HeroController {
     private final HeroService heroService;
 
@@ -18,29 +16,19 @@ public class HeroController {
         this.heroService = heroService;
     }
 
-    @RequestMapping(value = "/api/heroes", method = RequestMethod.POST)
-    @ResponseBody
+    @PostMapping(value = "/api/heroes")
     public Hero create(@RequestBody Hero hero) { // < 파라미터 확인 필요
-        Hero addHero = new Hero();
-        addHero.setName(hero.getName());
-        Hero result = heroService.join(addHero);
-
-        return result;
+        return heroService.join(hero);
     }
 
-    @RequestMapping(value = "/api/heroes", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping(value = "/api/heroes")
     public List<Hero> list() {
-        List<Hero> heroes = heroService.findMembers();
-        return heroes;
+        return heroService.findMembers();
     }
 
-    @RequestMapping(value = "/api/heroes/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/api/heroes/{id}")
     // 여기에 뭔가 응답을 주고 싶음, 삭제된 hero의 id 또는 name
-    public int delete(@PathVariable int id) {
-        Optional<Hero> hero = heroService.findById(id);
+    public void delete(@PathVariable int id) {
         heroService.remove(id);
-
-        return id;
     }
 }
